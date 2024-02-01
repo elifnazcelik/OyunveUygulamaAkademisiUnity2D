34f4d92182ba5ed8386c6a25ca6771a520d4121a
elifnazcelik <d.elifcelik@gmail.com>
2024-02-01 18:04:57 +0300

    :hammer: Update Character Controller
    Add flip and horizontal movement

--- OUA/Assets/Scripts/CharacterController.cs
+++ OUA/Assets/Scripts/CharacterController.cs
@@ -6,14 +6,16 @@ using UnityEngine.UIElements;
 
 public class CharacterController : MonoBehaviour
 {
-    public float speed = 0.0f; // We need to define it beforehand since we'll be using it.
+    public float speed = 1.0f; // We need to define it beforehand since we'll be using it.
     private Rigidbody2D r2d; //We should use Rigidbody and Animator in the script since it affects both the animation and the movement.
     private Animator animator;
     Vector3 charPosition; //to store position data
+    private SpriteRenderer spriteRenderer;
     [SerializeField] private GameObject camera; //We can see from inspector and add game object
 
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         r2d = GetComponent<Rigidbody2D>(); //caching r2d
         animator = GetComponent<Animator>(); //caching anim
         charPosition = transform.position; //current character position
@@ -26,17 +28,24 @@ public class CharacterController : MonoBehaviour
 
     private void Update() // per frame
     {
-        if(Input.GetKey(KeyCode.Space)) 
+        charPosition = new Vector3(charPosition.x + (Input.GetAxis("Horizontal")* speed * Time.deltaTime), charPosition.y);   //The character's position changing based on velocity with our pyhsic calculations
+        transform.position = charPosition; //We set the calculated new position as the character's position.
+        if(Input.GetAxis("Horizontal") == 0.0f) //idle animation
         {
-            speed = 1.0f;
+            animator.SetFloat("speed", 0.0f);
         }
         else
         {
-            speed = 0.0f;
+            animator.SetFloat(name: "speed", speed); //In the Animator, we define the "speed" parameter which we use in the script.
         }
-        charPosition = new Vector3(charPosition.x + (speed * Time.deltaTime), charPosition.y);   //The character's position changing based on velocity with our pyhsic calculations
-        transform.position = charPosition; //We set the calculated new position as the character's position.
-        animator.SetFloat(name: "speed", speed); //In the Animator, we define the "speed" parameter which we use in the script.
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(Input.GetAxis("Horizontal") < -0.01f)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 
     private void LateUpdate()
