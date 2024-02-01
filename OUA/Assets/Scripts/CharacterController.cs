6604a91a5ab9038c664cdf26c94f2c96c8a91594
elifnazcelik <d.elifcelik@gmail.com>
2024-02-01 17:41:37 +0300

    :hammer: Update Character Controller

--- OUA/Assets/Scripts/CharacterController.cs
+++ OUA/Assets/Scripts/CharacterController.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
 
 public class CharacterController : MonoBehaviour
 {
     public float speed = 0.0f; // We need to define it beforehand since we'll be using it.
     private Rigidbody2D r2d; //We should use Rigidbody and Animator in the script since it affects both the animation and the movement.
     private Animator animator;
+    Vector3 charPosition; //to store position data
+    [SerializeField] private GameObject camera; //We can see from inspector and add game object
 
     private void Start()
     {
         r2d = GetComponent<Rigidbody2D>(); //caching r2d
         animator = GetComponent<Animator>(); //caching anim
+        charPosition = transform.position; //current character position
     }
 
-    private void Update()
+    private void FixedUpdate() //physics, 50 fps(It is explained in detail in the "time calculation" section of the lecture notes.)
+    {
+       // r2d.velocity = new Vector2(speed, 0.0f); //Unity Pyhsics
+    }
+
+    private void Update() // per frame
     {
         if(Input.GetKey(KeyCode.Space)) 
         {
@@ -24,8 +34,13 @@ public class CharacterController : MonoBehaviour
         {
             speed = 0.0f;
         }
-
+        charPosition = new Vector3(charPosition.x + (speed * Time.deltaTime), charPosition.y);   //The character's position changing based on velocity with our pyhsic calculations
+        transform.position = charPosition; //We set the calculated new position as the character's position.
         animator.SetFloat(name: "speed", speed); //In the Animator, we define the "speed" parameter which we use in the script.
-        r2d.velocity = new Vector2(speed, 0.0f);
+    }
+
+    private void LateUpdate()
+    {
+        camera.transform.position = new Vector3(charPosition.x, charPosition.y, charPosition.z - 1.0f); ///The camera automatically follows the character. The camera starts one meter behind.
     }
 }
